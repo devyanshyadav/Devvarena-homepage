fd66c1c05bc182b8a0cc564f601607f415a88d35
Developer-Devyansh <devyanshyadav41@gmail.com>
2023-12-27 13:59:58 +0530

    padding given to header

--- app/page.js
+++ app/page.js
@@ -54,15 +54,27 @@ const page = () => {
 const devLink=`https://editor.devvarena.com/`
   return (
     <>
-      <section className="heroSection w-full h-[400px]  md:h-[500px] rounded-lg  md:rounded-3xl px-2 relative flex items-center  flex-col">
-        <header className="flex w-full items-center justify-between py-2">
+      <section className="heroSection w-full h-[400px]  md:h-[550px] rounded-lg  md:rounded-3xl px-2 relative flex items-center  flex-col">
+        <header className="flex w-full items-center justify-between p-4">
           <Link
             href={devLink}
             className="LOGO select-none text-3xl  font-semibold"
           >
             Devvarena{" "}
           </Link>
-          <Button color="primary" as={Link} href={devLink}>Try Now  <svg
+        <span className="space-x-2">
+        <Button color="primary" as={Link} href="#cssToolsDiv" size="sm">CSS Tools </Button>
+
+        <Button color="primary" as={Link} href={devLink} size="sm">Try Now  </Button>
+        </span>
+        </header>
+        <div className=" animate__animated animate__fadeIn md:w-5/6 flex items-center flex-col justify-center h-full space-y-2  transform translate-y-8 text-center">
+          <h1 className="text-4xl font-normal">
+            The Perfect Editor for Beginners
+          </h1>
+          <p className="font-light">Simple, Powerful, and Feature-Packed</p>
+          <span>
+            <Button color="primary" as={Link} href={devLink}>Try Editor Now <svg
                 class="rtl:rotate-180 w-3.5 h-3.5 ms-2"
                 aria-hidden="true"
                 xmlns="http://www.w3.org/2000/svg"
@@ -78,14 +90,6 @@ const devLink=`https://editor.devvarena.com/`
                   d="M1 5h12m0 0L9 1m4 4L9 9"
                 />
               </svg></Button>
-        </header>
-        <div className=" animate__animated animate__fadeIn md:w-5/6 flex items-center flex-col justify-center h-full space-y-2  transform translate-y-8 text-center">
-          <h1 className="text-4xl font-normal">
-            The Perfect Editor for Beginners
-          </h1>
-          <p className="font-light">Simple, Powerful, and Feature-Packed</p>
-          <span>
-            <Button color="primary" as={Link} href={devLink}>Try Editor Now</Button>
           </span>
           <Image
             src={heroImg}
@@ -96,7 +100,7 @@ const devLink=`https://editor.devvarena.com/`
           />
         </div>
       </section>
-      <div className="w-full h-[100px] md:h-[200px] flex items-end justify-center">
+      <div className="w-full h-[100px] md:h-[250px] flex items-end justify-center">
         <Link  href={devLink} className="border rounded-3xl px-5 text-sm border-primary flex items-center justify-center p-1">Kickstart your first project now<svg
                 class="rtl:rotate-180 w-3.5 h-3.5 ms-2"
                 aria-hidden="true"
@@ -146,22 +150,22 @@ const devLink=`https://editor.devvarena.com/`
           className="w-full row-span-2"
         />
       </section>
-      <section className="flex items-center justify-center flex-col mt-4 text-center space-y-3">
+      <section id="cssToolsDiv" className="flex items-center justify-center flex-col mt-4 text-center space-y-3">
         <span>
-          <h1 className="text-4xl font-normal">Supercharge Your Web Dev</h1>
+          <h1 className="text-3xl font-normal">Supercharge Your Web Dev</h1>
           <p className="font-light text-primary text-xl">
             with Essential CSS Tools
           </p>
         </span>
-        <div className="grid grid-cols-1 md:flex flex-wrap items-center justify-center gap-2 p-5 md:p-9 w-full text-white">
+        <div  className="grid grid-cols-1 md:flex flex-wrap items-center justify-center gap-2 p-5 md:p-9 w-full text-white">
          {
           cssTools.map((elem, i)=>(
-            <div class="p-6 bg-tertiary border md:block grid grid-cols-2 gap-2  border-gray-600 rounded-lg shadow hover:border-primary " key={i}>
-            <a href="#">
+            <div class="p-6 hover:bg-tertiary border md:block grid grid-cols-2 gap-2  border-gray-600 rounded-lg shadow" key={i}>
+            <Link href={elem.link}>
               <h5 class="mb-2 text-start text-sm md:text-2xl font-bold ">
                 {elem.name}
               </h5>
-            </a>
+            </Link>
             <Button
       href={elem.link}
       size="sm"
