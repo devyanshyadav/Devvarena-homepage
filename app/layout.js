c0e5cb13713df76e834c12c8950fe41c7eba343e
Developer-Devyansh <devyanshyadav41@gmail.com>
2024-01-13 22:36:12 +0530

    meta data added

--- app/layout.js
+++ app/layout.js
@@ -1,21 +1,48 @@
-import { Inter } from 'next/font/google'
-import './globals.css'
+import { Inter } from "next/font/google";
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Devvarena',
-  description: 'The Perfect Editor for Beginners | Simple, Powerful, and Feature-Packed',
-}
+  title: "Devvarena - The Perfect Frontend Editor for Beginners",
+  description:
+    "Devvarena is a simple, powerful, and feature-packed frontend editor designed specifically for beginners. It includes CSS tools, a JavaScript editor, an HTML editor, and a CSS editor. It also provides a JavaScript console for debugging and testing your code. Start coding and exploring the world of frontend development with Devvarena",
+  keywords: [
+    "codepen",
+    "sandbox",
+    "replit",
+    "devyansh",
+    "frontend editor",
+    "CSS tools",
+    "JavaScript editor",
+    "HTML editor",
+    "CSS editor",
+    "JS console",
+    "online editor",
+    " beginner-friendly editor",
+    "frontend editor for beginners",
+    "best frontend editor for learning",
+    "easy-to-use frontend editor",
+    "beginner-friendly css tools",
+    "javascript editor with console",
+    "html and css editor for beginners",
+    "simple frontend editor",
+    "powerful frontend editor",
+    "feature-packed frontend editor",
+    "learn html and css online",
+    "interactive frontend coding",
+    "web development for beginners",
+  ],
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <main className='w-screen h-screen overflow-y-scroll bg-secondary max-w-6xl md:px-3'>
-        {children}
+        <main className="w-screen h-screen overflow-y-scroll bg-secondary max-w-6xl md:px-3">
+          {children}
         </main>
       </body>
     </html>
-  )
+  );
 }
